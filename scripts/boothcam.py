dce7810e0292a1e2d92f4965f5ebb6aa9dc01c26
Steve Jernigan <sjernigan@reverbnation.com>
2016-09-24 19:57:01 -0400

    Move image to center on four

--- scripts/boothcam.py
+++ scripts/boothcam.py
@@ -162,7 +162,7 @@ def snap(can, countdown1, effect='None'):
                 #                             custom.logo)
                 size = snapshot.size
                 logo_size = custom.logo.size
-                yoff = size[1] - logo_size[1] //2
+                yoff = (size[1] - logo_size[1]) // 2
                 xoff = (size[0] - logo_size[0]) // 2
                 snapshot.paste(custom.logo,(xoff, yoff),
                                custom.logo)
