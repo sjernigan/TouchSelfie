9e6968c8af7fe5c8a58cc5f133869703207c15cf
Steve Jernigan <sjernigan@reverbnation.com>
2016-09-24 21:52:32 -0400

    make name generic

--- scripts/photobooth_gui.py
+++ scripts/photobooth_gui.py
@@ -352,7 +352,7 @@ force_snap(countdown1=0)
 root.after(200, check_and_snap)
 if custom.SIGN_ME_IN:
     root.after(custom.oauth2_refresh_period, refresh_oauth2_credentials)
-root.wm_title("Wyolum Photobooth")
+root.wm_title("Photobooth")
 etext.focus_set()
 # etext.bind("<Enter>", sendPic)
 on_rgb_change()
